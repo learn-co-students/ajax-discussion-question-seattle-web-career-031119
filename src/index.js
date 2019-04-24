a2458d25200f97b1218ed5cb312157c6184d90f9
Rylan Bauermeister <apple@MacBook-Pro-4.local>
2019-04-24 09:35:28 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  let button = document.getElementById('get-user-button');
+  button.addEventListener('click', () => {
+    fetch('https://randomuser.me/api/')
+      .then(res => res.json())
+      .then(putName);
+  })
+
+  function putName(json){
+    console.log(json);
+    let result = json.results[0];
+    fullname.textContent = `${result.name.first} ${result.name.last}`
+  }
 });
