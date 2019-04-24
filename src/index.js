c0f591673970b2594dde5abf170367b5d1be2fbb
Sofia <jonssonsofia93@gmail.com>
2019-04-24 09:48:21 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+const button = document.getElementById("button")
+button.addEventListener('click', getInfo)
+
+  function getInfo(){
+          fetch('https://randomuser.me/api/')
+            .then( res => res.json() )
+            .then( data => {
+              console.log(data.results[0])
+            fullname.textContent = data.results[0].name.title + "." + " "+ data.results[0].name.first + " " + data.results[0].name.last })
+  }
+
+  fetchData()
+
 });
-//doesn't matter what order the DOMContentLoaded is mentioned in the JS file
